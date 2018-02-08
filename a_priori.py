47492037e3491f16969748a53f3caa2d03243454
Zach <zachary.rudzinski@gmail.com>
2018-02-08 17:47:02 -0500

    Bug fix, pruning of infrequent items
    Bug fixes:
    - Pulled some code in the a_priori function into the for loop where it belongs
    	- ie: The recording of each basket item (tmp_items) into the hashtable (items)
    - Fixed an issue where we would ignore how many baskets to look through when counting items in pass 1
    
    Pruning of infrequent items:
    - Added functionality to remove items which were not frequent (ie: they showed up in less than $supportThreshold % of baskets).

--- a_priori.py
+++ a_priori.py
@@ -1,24 +1,41 @@
 import datetime
 from collections import defaultdict
 
-def a_priori(input_file, num_baskets, support_threshold):
-    # Our hashmap/dict of possibly frequent items
-    # Default value of any basket will always be 0
-    items = defaultdict(lambda: 0)
-    with open(input_file, 'r') as fp:
-        # Fetching the values from the line (as strings)
-        line = fp.readline()
-        tmp_items = line.split(' ')[: -1]
 
+def a_priori(input_file, num_baskets, support_threshold):
     # -- Pass 1 --
     # Any frequent pairs would have to contain items that would be also considered frequent
     # Therefore, we can go through the baskets and eliminate all items which are not considered "frequent"
     # No frequent pairs would contain items which are not frequent
 
-    # Making note of each item's appearance in a basket
-    for i in tmp_items:
-        items[i] += 1
-    num_items = items.__len__()
+    # Our hashmap/dict of possibly frequent items
+    # Default value of any basket will always be 0
+    items = defaultdict(lambda: 0)
+    with open(input_file, 'r') as fp:
+        # Reading up to $num_basket baskets
+        for basket_index in range(num_baskets):
+            # Fetching the values from the line (as strings), and removing the newline character at the end
+            line = fp.readline()
+            tmp_items = line.split(' ')[: -1]
+
+            # Making note of each item's appearance in a basket
+            for i in tmp_items:
+                items[i] += 1
+
+    # Recording the total number of unique items (ie: union)
+    # I remember needing this for something later on, but I can't remember if I need the number of items before or after
+    # the pruning of infrequent items
+    # TODO: What's this for?
+    # num_items = items.__len__()
+
+    # Removing items which are not frequent
+    # Since any item in a frequent pair would have to occur in more than $support_threshold baskets,
+    #   we can toss out any items which have a frequency less than a specified amount
+    min_required_num_items = num_baskets * support_threshold
+    for key, value in items:
+        # If this item is NOT frequent, we can safely toss it out
+        if value < min_required_num_items:
+            items.pop(key)
 
 
 def main():
