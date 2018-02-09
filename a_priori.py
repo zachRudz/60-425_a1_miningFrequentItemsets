ea2d3e76039bb25928d0776ab6821b611fc305a1
Zach <zachary.rudzinski@gmail.com>
2018-02-08 20:04:27 -0500

    Finished implimenting A Priori. Debug statements.
    - Pass 2 now implimented
    	- Given a set of items that are frequent, all itemsets containing these items are generated.
    
    - Final verification, pruning non-frequent itemsets now implimented
    	- Not all itemsets which contain frequent items are frequent itemsets.
    	- This step verifies that the itemsets from pass 2 are actually frequent.
    
    - Fixed timer so that it doesn't start until AFTER the user gives their input.
    	- This saves up to 5 seconds on the clock, due to user input delay.

--- a_priori.py
+++ a_priori.py
@@ -1,6 +1,7 @@
 import datetime
 from collections import defaultdict
 
+DEBUG = True
 
 def a_priori(input_file, num_baskets, support_threshold):
     # -- Pass 1 --
@@ -10,6 +11,7 @@ def a_priori(input_file, num_baskets, support_threshold):
 
     # Our hashmap/dict of possibly frequent items
     # Default value of any basket will always be 0
+    global key_pair
     items = defaultdict(lambda: 0)
     with open(input_file, 'r') as fp:
         # Reading up to $num_basket baskets
@@ -22,40 +24,66 @@ def a_priori(input_file, num_baskets, support_threshold):
             for i in tmp_items:
                 items[i] += 1
 
-    # Recording the total number of unique items (ie: union)
-    # I remember needing this for something later on, but I can't remember if I need the number of items before or after
-    # the pruning of infrequent items
-    # TODO: What's this for?
-    # num_items = items.__len__()
+    if DEBUG:
+        print("Number of unique items: {}".format(len(items)))
 
-    # Removing items which are not frequent
-    # Since any item in a frequent pair would have to occur in more than $support_threshold baskets,
-    #   we can toss out any items which have a frequency less than a specified amount
-    min_required_num_items = num_baskets * support_threshold
-    for key, value in items:
-        # If this item is NOT frequent, we can safely toss it out
-        if value < min_required_num_items:
-            items.pop(key)
+    # -- Pass 2 --
+    # Now that we have the list of items which are frequent, we can find each combination of items,
+    # where both of the items in the pair are frequent. Therefore, through monotonicity, that pair would be frequent.
+    frequent_pairs = defaultdict(lambda: 0)
+    min_required_occurrences = num_baskets * support_threshold
+    if DEBUG:
+        print("Minimum number of occurrences to be considered frequent: {}".format(min_required_occurrences))
+
+    with open(input_file, 'r') as fp:
+        # Reading up to $num_basket baskets
+        for basket_index in range(num_baskets):
+            # Fetching the values from the line (as strings), and removing the newline character at the end
+            line = fp.readline()
+            tmp_items = line.split(' ')[: -1]
+
+            # Recording the number of frequent pairs in which both items are frequent
+            # Fetching all combinations of this basket's items
+            for a in range(len(tmp_items) - 1):
+                for b in range(a + 1, len(tmp_items)):
+                    # Making sure both items are frequent
+                    if items[tmp_items[a]] > min_required_occurrences and items[tmp_items[b]] > min_required_occurrences:
+                        frequent_pairs[(a, b)] += 1
+
+    # -- Verification --
+    # Now that we have a list of "frequent pairs", in which both of its items are frequent, we need to verify that
+    # each of these frequent pairs actually has a support greater than the threshold.
+    prev_total_frequent_pairs = len(frequent_pairs)
+    pairs_to_pop = []
+    for key_pair in frequent_pairs:
+        if frequent_pairs[key_pair] < min_required_occurrences:
+            pairs_to_pop.append(key_pair)
+
+    # Going through and popping all of these pairs, since they're not frequent
+    # Doing this outside the previous for loop because python doesn't like it when you modify the size of an iterable
+    # object while iterating over it
+    for pair in pairs_to_pop:
+        if DEBUG:
+            print("Popping {}: {} occurrences.".format(pair, frequent_pairs[pair]))
+        frequent_pairs.pop(pair)
+
+    if DEBUG:
+        print("Frequent pairs before verification: {}".format(prev_total_frequent_pairs))
+        print("Frequent pairs after verification: {}".format(len(frequent_pairs)))
+
+        print("-- Frequent pairs --")
+        for key_pair in frequent_pairs:
+            print("{}: {}".format(key_pair, frequent_pairs[key_pair]))
+
+    return frequent_pairs
 
 
 def main():
-    # Starting the clock
-    start_time = datetime.datetime.now()
-    print("Start time: {}".format(start_time))
-
-    # Finding the number of lines in our file
-    input_file = 'retail.txt'
-    total_num_lines = 0
-    with open(input_file, 'r') as fp:
-        while(fp.readline()):
-            total_num_lines += 1
-
     # Reading chunk size from the terminal
     chunk_percentage = float(input("How much of the file do you want to look through? (In percentage format pls): "))
     if chunk_percentage > 1 or chunk_percentage < 0:
         print("Error: The chunk percentage must be between 0.0 and 1.0.")
         return
-    num_baskets = int(total_num_lines * chunk_percentage)
 
     # Reading support threshold from the terminal
     support_threshold = float(input("What is your support threshold? (In percentage format pls): "))
@@ -63,12 +91,30 @@ def main():
         print("Error: The support threshold must be between 0.0 and 1.0.")
         return
 
+    # Starting the clock
+    start_time = datetime.datetime.now()
+    print("Start time: {}".format(start_time))
+    print("")
+
+    # Finding the number of lines in our file
+    input_file = 'retail.txt'
+    total_num_lines = 0
+    with open(input_file, 'r') as fp:
+        while fp.readline():
+            total_num_lines += 1
+
+    # Calculating how many baskets (read: lines) we're going through
+    num_baskets = int(total_num_lines * chunk_percentage)
+    if DEBUG:
+        print("Number of baskets: {}".format(num_baskets))
+
     # Doing a_priori things
-    a_priori(input_file, num_baskets, support_threshold)
+    frequent_pairs = a_priori(input_file, num_baskets, support_threshold)
 
     # Stopping the clock
     end_time = datetime.datetime.now()
     diff_time = end_time - start_time
+    print("")
     print("Start time: {}".format(start_time))
     print("End time: {}".format(end_time))
     print("Total elapsed time: {}".format(diff_time))
